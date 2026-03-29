53cfe5cc6981be909f99daf6d8eef9dd13edac9a
mateus-sartorio <mateussartorio39@gmail.com>
2026-03-28 21:02:16 -0300

    chore: reorganize directory structure

--- src/.antlr/pascal_grammar.java
+++ src/.antlr/pascal_grammar.java
@@ -1,4 +1,4 @@
-// Generated from /home/mateus/Faculdade/compilers/pascal-to-mips-compiler/ANTLR/pascal_grammar.g by ANTLR 4.13.1
+// Generated from /home/mateus/Faculdade/compilers/pascal-to-mips-compiler/src/pascal_grammar.g by ANTLR 4.13.1
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
