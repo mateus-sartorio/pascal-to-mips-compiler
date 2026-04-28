eca802df4e0dac43f21f8ef3b2e52be5c640161a
Gianluca-Mantovaneli <gianlucamantovaneli@gmail.com>
2026-04-28 14:00:52 -0300

    feat: add invalid test cases for lexer to improve error handling

---
+++ tests/lexer/invalid/2-number-bad-format/source.pas
@@ -0,0 +1,8 @@
+program NumberBadFormat;
+var
+  r: real;
+begin
+  r := 10.; 
+  r := .5;
+  r := 1.2e;
+end.
---
+++ tests/lexer/invalid/3-bad-format-strings/source.pas
@@ -0,0 +1,7 @@
+program BadFormatStrings;
+var
+  s: string;
+begin
+  s := 'Esta string começa aqui
+  e tenta terminar aqui';
+end.
