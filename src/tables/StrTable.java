6ae2bd9e9a1a2aa82c76e04b245e99d913f71d70
Gianluca <gianlucamantovaneli@gmail.com>
2026-05-16 13:02:52 -0300

    feat: implement symbol table and type enumeration classes

---
+++ src/tables/StrTable.java
@@ -0,0 +1,14 @@
+package tables;
+
+import
+
+
+@SupressWarnings("serial")
+public final class StrTable {
+    private final Map<String, Integer> table = new LinkedHashMap<>();
+    private int count = 0;
+
+    public boolean add(String str) {
+        if(table.contains())
+    }
+}
