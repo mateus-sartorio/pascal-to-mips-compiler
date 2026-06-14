28cf766d3ebd9917b85ef5a89c473e1bb97622f4
mateus-sartorio <mateussartorio39@gmail.com>
2026-06-14 14:15:32 -0300

    chore: remove uneeded files

--- tests/ast/valid/teste-para-montar-ast/source.pas
+++ tests/ast/valid/teste-para-montar-ast/source.pas
@@ -1,33 +1,33 @@
 program DeepExpression;
 var
-res,a,b: integer;
-x,y,z: string;
+  res,a,b: integer;
+  x,y,z: string;
 
 function testeFunction(aParamFunc: integer; bParamFunc: real):integer;
 var
-xLocalVarFunc: integer;
-yLocalVarFunc: real;
+  xLocalVarFunc: integer;
+  yLocalVarFunc: real;
 begin
-xLocalVarFunc := aParamFunc + 10;
-yLocalVarFunc := bParamFunc * 2.5;
-res := xLocalVarFunc + round(yLocalVarFunc);
-testeFunction := res;
+  xLocalVarFunc := aParamFunc + 10;
+  yLocalVarFunc := bParamFunc * 2.5;
+  res := xLocalVarFunc + round(yLocalVarFunc);
+  testeFunction := res;
 end;
 
 procedure testeProcedure(aParamProc: integer; bParamProc: real);
 var
-xLocalVarProc: integer;
-yLocalVarProc: real;
+  xLocalVarProc: integer;
+  yLocalVarProc: real;
 begin
-xLocalVarProc := aParamProc - 5;
-yLocalVarProc := bParamProc / 2.0;
-res := xLocalVarProc + round(yLocalVarProc);
+  xLocalVarProc := aParamProc - 5;
+  yLocalVarProc := bParamProc / 2.0;
+  res := xLocalVarProc + round(yLocalVarProc);
 end;
 
 begin
-a := 5;
-b := 10;
-x := 'Hello';
-y := 'World';
-z := x + ' ' + y;   
+  a := 5;
+  b := 10;
+  x := 'Hello';
+  y := 'World';
+  z := x + ' ' + y;   
 end.
