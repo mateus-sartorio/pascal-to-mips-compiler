a6ff9ff26d947911d7639086738ebfa026eed191
mateus-sartorio <mateussartorio39@gmail.com>
2026-06-16 15:43:40 -0300

    wip

--- src/ast/types/AstNode.java
+++ src/ast/types/AstNode.java
@@ -2,7 +2,7 @@ package ast.types;
 
 public abstract class AstNode {
   public String getDotNotationIdentifier() {
-    return "\"" +getClass().getSimpleName() + "#" + System.identityHashCode(this) + "\"";
+    return "\"" + getClass().getSimpleName() + "#" + System.identityHashCode(this) + "\"";
   }
 
   public abstract String toDotNotation();
